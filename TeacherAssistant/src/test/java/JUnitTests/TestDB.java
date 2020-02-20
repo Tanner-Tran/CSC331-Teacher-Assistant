6af67a767622016177f88e26b5bc36928cd2656c
sirkm <sirkm@10.20.120.45>
2020-02-20 11:02:03 -0600

    I added a JUNIT test for the basic database functions.

---
+++ TeacherAssistant/src/test/java/JUnitTests/TestDB.java
@@ -0,0 +1,59 @@
+package JUnitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import Model.Database;
+
+class TestDB {
+	@SuppressWarnings("static-access")
+	@Test
+	public void test() throws Exception {
+		Database dummy = new Database();
+		
+		try
+		{
+			dummy.openDB();
+			System.out.println("Connection found.");
+		}
+		catch (Exception test)
+		{
+			System.out.println("Connection error.");
+			System.exit(0);
+		}
+		dummy.removeTeacherUser("H Whit");
+		DatabaseModifyTest(dummy);
+		DatabaseLogin(dummy);
+	}
+	
+	@SuppressWarnings("static-access")
+	public void DatabaseModifyTest(Database demo) throws SQLException
+	{
+		demo.addTeacherUser("Whitston", "Howard", "H Whit", "password");
+		
+		if (demo.checkIfUsernameAvailable("H Whit"))
+		{
+			System.out.println("Username exists.");
+		}
+		
+		demo.removeTeacherUser("H Whit");
+	}
+	
+	@SuppressWarnings("static-access")
+	public void DatabaseLogin(Database test) throws SQLException
+	{
+		if (test.validateLogin("L. Larry", "dataStructure"))
+		{
+			System.out.println("Correct password login was successful.");
+		}
+		
+		if (!test.validateLogin("L. Larry", "data"))
+		{
+			System.out.println("Correct password login failed.");
+		}
+	}
+
+}
