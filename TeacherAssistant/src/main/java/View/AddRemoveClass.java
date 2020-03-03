2f99c7353ad6d882943edf400707de5a540b88c9
andrew <andrew@192.168.0.114>
2020-03-02 18:57:57 -0600

    added add/remove class and add/remove student

---
+++ TeacherAssistant/src/main/java/View/AddRemoveClass.java
@@ -0,0 +1,119 @@
+package View;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+public class AddRemoveClass {
+	private static Text classTitleTextbox;
+	private static Text classCodeTextbox;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Display display = Display.getDefault();
+		Shell shell = new Shell();
+		shell.setSize(471, 327);
+		shell.setText("Add / Remove Class");
+		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		Composite composite = new Composite(shell, SWT.BORDER);
+		GridLayout gl_composite = new GridLayout(3, false);
+		gl_composite.horizontalSpacing = 10;
+		gl_composite.marginLeft = 25;
+		gl_composite.marginRight = 30;
+		gl_composite.marginTop = 25;
+		gl_composite.verticalSpacing = 15;
+		composite.setLayout(gl_composite);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		
+		Button addRadioBtn = new Button(composite, SWT.RADIO);
+		addRadioBtn.setSelection(true);
+		addRadioBtn.setText("Add");
+		new Label(composite, SWT.NONE);
+		
+		Button removeRadioBtn = new Button(composite, SWT.RADIO);
+		removeRadioBtn.setText("Remove");
+		
+		Label lblNewLabel = new Label(composite, SWT.NONE);
+		GridData gd_lblNewLabel = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_lblNewLabel.widthHint = 54;
+		lblNewLabel.setLayoutData(gd_lblNewLabel);
+		lblNewLabel.setText("Class Title");
+		
+		classTitleTextbox = new Text(composite, SWT.BORDER);
+		GridData gd_classTitleTextbox = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_classTitleTextbox.widthHint = 105;
+		classTitleTextbox.setLayoutData(gd_classTitleTextbox);
+		
+		Combo classDropdown = new Combo(composite, SWT.NONE);
+		classDropdown.setForeground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_NORMAL_SHADOW));
+		classDropdown.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		classDropdown.setText("---Classes---");
+		classDropdown.add("Class 1");
+		
+		Label lblClassCode = new Label(composite, SWT.NONE);
+		lblClassCode.setText("Class Code");
+		
+		classCodeTextbox = new Text(composite, SWT.BORDER);
+		GridData gd_classCodeTextbox = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_classCodeTextbox.widthHint = 105;
+		classCodeTextbox.setLayoutData(gd_classCodeTextbox);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		
+		Button addClassButton = new Button(composite, SWT.NONE);
+		GridData gd_addClassButton = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_addClassButton.verticalIndent = 15;
+		addClassButton.setLayoutData(gd_addClassButton);
+		addClassButton.setText("Add Class");
+		
+		Button removeClassBtn = new Button(composite, SWT.NONE);
+		GridData gd_removeClassBtn = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_removeClassBtn.verticalIndent = 15;
+		removeClassBtn.setLayoutData(gd_removeClassBtn);
+		removeClassBtn.setText("Remove Class");
+
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			
+			if(removeRadioBtn.getSelection()) {
+				classDropdown.setEnabled(true);
+				removeClassBtn.setEnabled(true);
+				
+				classTitleTextbox.setEnabled(false);
+				classCodeTextbox.setEnabled(false);
+				addClassButton.setEnabled(false);
+			}
+			
+			if(addRadioBtn.getSelection()) {
+				classTitleTextbox.setEnabled(true);
+				classCodeTextbox.setEnabled(true);
+				addClassButton.setEnabled(true);
+				
+				classDropdown.setEnabled(false);
+				removeClassBtn.setEnabled(false);
+			}
+			
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+}
---
+++ TeacherAssistant/src/main/java/View/AddRemoveStudent.java
@@ -0,0 +1,154 @@
+package View;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+public class AddRemoveStudent {
+	private static Text firstNameTb;
+	private static Text lastNameTb;
+	private static Text studentID;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Display display = Display.getDefault();
+		Shell shell = new Shell();
+		shell.setSize(541, 367);
+		shell.setText("Add / Remove Student");
+		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		Composite composite = new Composite(shell, SWT.BORDER);
+		GridLayout gl_composite = new GridLayout(3, false);
+		gl_composite.horizontalSpacing = 10;
+		gl_composite.marginLeft = 25;
+		gl_composite.marginRight = 30;
+		gl_composite.marginTop = 25;
+		gl_composite.verticalSpacing = 15;
+		composite.setLayout(gl_composite);
+		
+		Button addRadioBtn = new Button(composite, SWT.RADIO);
+		addRadioBtn.setSelection(true);
+		addRadioBtn.setText("Add");
+		new Label(composite, SWT.NONE);
+		
+		Button removeRadioBtn = new Button(composite, SWT.RADIO);
+		removeRadioBtn.setText("Remove");
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		
+		Combo classesDropdown = new Combo(composite, SWT.NONE);
+		classesDropdown.setForeground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_NORMAL_SHADOW));
+		GridData gd_classesDropdown = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_classesDropdown.widthHint = 115;
+		classesDropdown.setLayoutData(gd_classesDropdown);
+		classesDropdown.setText("---Classes---");
+		
+		Combo classesDropdown2 = new Combo(composite, SWT.NONE);
+		classesDropdown2.setForeground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_NORMAL_SHADOW));
+		classesDropdown2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		classesDropdown2.add("Class 1");
+		classesDropdown2.setText("---Classes---");
+		
+		Label lblFirstName = new Label(composite, SWT.NONE);
+		GridData gd_lblFirstName = new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1);
+		gd_lblFirstName.widthHint = 100;
+		lblFirstName.setLayoutData(gd_lblFirstName);
+		lblFirstName.setText("First Name");
+		
+		firstNameTb = new Text(composite, SWT.BORDER);
+		GridData gd_firstNameTb = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_firstNameTb.widthHint = 128;
+		firstNameTb.setLayoutData(gd_firstNameTb);
+		
+		Combo studentsDropdown = new Combo(composite, SWT.NONE);
+		studentsDropdown.setForeground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_NORMAL_SHADOW));
+		studentsDropdown.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		studentsDropdown.setText("---Students---");
+		
+		Label lblNewLabel = new Label(composite, SWT.NONE);
+		GridData gd_lblNewLabel = new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1);
+		gd_lblNewLabel.widthHint = 99;
+		lblNewLabel.setLayoutData(gd_lblNewLabel);
+		lblNewLabel.setText("Last Name");
+		
+		lastNameTb = new Text(composite, SWT.BORDER);
+		GridData gd_lastNameTb = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_lastNameTb.widthHint = 128;
+		lastNameTb.setLayoutData(gd_lastNameTb);
+		new Label(composite, SWT.NONE);
+		
+		Label lblClassCode = new Label(composite, SWT.NONE);
+		GridData gd_lblClassCode = new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1);
+		gd_lblClassCode.widthHint = 90;
+		lblClassCode.setLayoutData(gd_lblClassCode);
+		lblClassCode.setText("Student ID");
+		
+		studentID = new Text(composite, SWT.BORDER);
+		GridData gd_studentID = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_studentID.widthHint = 128;
+		studentID.setLayoutData(gd_studentID);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		
+		Button addClassButton = new Button(composite, SWT.NONE);
+		GridData gd_addClassButton = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_addClassButton.verticalIndent = 15;
+		addClassButton.setLayoutData(gd_addClassButton);
+		addClassButton.setText("Add Student");
+		
+		Button removeClassBtn = new Button(composite, SWT.NONE);
+		GridData gd_removeClassBtn = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_removeClassBtn.verticalIndent = 15;
+		removeClassBtn.setLayoutData(gd_removeClassBtn);
+		removeClassBtn.setText("Remove Student");
+
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			
+			if(removeRadioBtn.getSelection()) {
+				classesDropdown2.setEnabled(true);
+				removeClassBtn.setEnabled(true);
+				studentsDropdown.setEnabled(true);
+				
+				studentID.setEnabled(false);
+				classesDropdown.setEnabled(false);
+				firstNameTb.setEnabled(false);
+				lastNameTb.setEnabled(false);
+				addClassButton.setEnabled(false);
+			}
+			
+			if(addRadioBtn.getSelection()) {
+				studentID.setEnabled(true);
+				classesDropdown.setEnabled(true);
+				firstNameTb.setEnabled(true);
+				lastNameTb.setEnabled(true);
+				addClassButton.setEnabled(true);
+				
+				studentsDropdown.setEnabled(false);
+				classesDropdown2.setEnabled(false);
+				removeClassBtn.setEnabled(false);
+			}
+			
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+}
