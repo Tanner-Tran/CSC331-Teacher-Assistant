d629d5f5b5826ce98bc096c1a5e865149104ec34
andrew <andrew@192.168.0.114>
2020-04-19 18:51:32 -0500

    Added log behavior and its class/student selection

---
+++ TeacherAssistant/src/main/java/View/ClassStudentSelection.java
@@ -0,0 +1,92 @@
+package View;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+public class ClassStudentSelection {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			ClassStudentSelection window = new ClassStudentSelection();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(308, 224);
+		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		Composite composite = new Composite(shell, SWT.NONE);
+		GridLayout gl_composite = new GridLayout(3, false);
+		gl_composite.marginLeft = 25;
+		gl_composite.verticalSpacing = 20;
+		gl_composite.marginTop = 30;
+		composite.setLayout(gl_composite);
+		
+		Label lblClass = new Label(composite, SWT.NONE);
+		lblClass.setText("Class :");
+		new Label(composite, SWT.NONE);
+		
+		CCombo classDropdown = new CCombo(composite, SWT.BORDER | SWT.READ_ONLY);
+		GridData gd_classDropdown = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_classDropdown.widthHint = 138;
+		classDropdown.setLayoutData(gd_classDropdown);
+		
+		Label lblDate = new Label(composite, SWT.NONE);
+		lblDate.setText("Student :");
+		new Label(composite, SWT.NONE);
+		
+		CCombo combo = new CCombo(composite, SWT.BORDER | SWT.READ_ONLY);
+		GridData gd_combo = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_combo.widthHint = 138;
+		combo.setLayoutData(gd_combo);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		
+		Button okBtn = new Button(composite, SWT.NONE);
+		GridData gd_okBtn = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_okBtn.widthHint = 58;
+		okBtn.setLayoutData(gd_okBtn);
+		okBtn.setText("OK");
+
+
+
+	}
+
+}
