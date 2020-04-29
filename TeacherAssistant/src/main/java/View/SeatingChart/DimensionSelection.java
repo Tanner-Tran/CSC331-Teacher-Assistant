4b66daa5b02c9defb1534b3a1013a40d4fa7f961
andrew <andrew@DESKTOP-0BN9OVN.attlocal.net>
2020-04-28 23:57:23 -0500

    seating chart stuff

---
+++ TeacherAssistant/src/main/java/View/SeatingChart/DimensionSelection.java
@@ -0,0 +1,116 @@
+package View.SeatingChart;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+import Controller.DBController;
+
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class DimensionSelection {
+
+	protected Shell shell;
+	private Text columnsText;
+	private Text rowsText;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			DimensionSelection window = new DimensionSelection();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(244, 206);
+		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
+		shell.setText("Input Information");
+		
+		org.eclipse.swt.graphics.Rectangle bds = shell.getMonitor().getBounds();
+		Point p = shell.getSize();
+		int nLeft = (bds.width - p.x) / 2;
+		int nTop = (bds.height - p.y) / 2;
+		shell.setBounds(nLeft, nTop, p.x, p.y);
+		
+		Composite composite = new Composite(shell, SWT.NONE);
+		GridLayout gl_composite = new GridLayout(3, false);
+		gl_composite.marginLeft = 25;
+		gl_composite.verticalSpacing = 20;
+		gl_composite.marginTop = 30;
+		composite.setLayout(gl_composite);
+		
+		Label lbl1 = new Label(composite, SWT.NONE);
+		lbl1.setLayoutData(new GridData(SWT.LEFT, SWT.BOTTOM, false, false, 1, 1));
+		lbl1.setText("Rows");
+		new Label(composite, SWT.NONE);
+		
+		rowsText = new Text(composite, SWT.BORDER);
+		GridData gd_rowsText = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_rowsText.widthHint = 25;
+		rowsText.setLayoutData(gd_rowsText);
+		
+		Label lbl2 = new Label(composite, SWT.NONE);
+		lbl2.setText("Columns");
+		new Label(composite, SWT.NONE);
+		
+		columnsText = new Text(composite, SWT.BORDER);
+		GridData gd_columnsText = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_columnsText.widthHint = 25;
+		columnsText.setLayoutData(gd_columnsText);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		
+		Button okBtn = new Button(composite, SWT.NONE);
+		okBtn.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				String rows = rowsText.getText();
+				String columns = columnsText.getText();
+				
+				CreateSeatChart window = new CreateSeatChart();
+				window.open();
+			}
+		});
+		GridData gd_okBtn = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_okBtn.widthHint = 58;
+		okBtn.setLayoutData(gd_okBtn);
+		okBtn.setText("OK");
+
+	}
+
+}
---
+++ TeacherAssistant/src/main/java/View/SeatingChart/ViewSeatChart.java
@@ -0,0 +1,148 @@
+package View.SeatingChart;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.text.TableView.TableRow;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.ScrollPaneConstants;
+
+public class ViewSeatChart {
+
+	JFrame seatChart;
+	JTable table;
+	DefaultTableModel model;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			ViewSeatChart window = new ViewSeatChart();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		createContents();
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+
+		int rows = 4;
+		int columns = 4;
+		
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        
+        double width = screenSize.getWidth();
+        double tableWidth = columns * 150;
+        double tableHeight = (rows * 27) + 37;
+        
+		seatChart = new JFrame();
+		
+		seatChart.setTitle("Seat Chart");
+		
+		if (tableWidth > width) {
+			seatChart.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		}
+		else {
+			seatChart.setSize((int)tableWidth + 30, (int)tableHeight + 130);
+			seatChart.setLocationRelativeTo(null);
+		}
+		
+        
+		 
+		 Object[] columnsArray = new Object[columns];
+		 Object[][] rowsArray = new Object[rows][columns];
+
+        
+        DefaultTableModel tableModel = new DefaultTableModel();
+        table = new JTable(new DefaultTableModel(rowsArray, columnsArray));
+    	table.setRowHeight(27);
+    	table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+    	
+
+        for (int i = 0; i < columns; i++) {
+        	TableColumn column = table.getColumnModel().getColumn(i);
+            JComboBox comboBox = new JComboBox();
+            comboBox.addItem("Brandon Nguyen");
+            column.setCellEditor(new DefaultCellEditor(comboBox));
+            column.setMinWidth(150);
+            column.setMaxWidth(150);
+            
+            column.setCellRenderer(new CheckBoxCellRenderer(comboBox));
+            table.repaint();
+            
+        }   
+        
+        table.setBounds(20, 20, 240, 300);
+        table.getTableHeader().setReorderingAllowed(false);
+        seatChart.getContentPane().setLayout(null);
+        
+        // adding it to JScrollPane 
+        JScrollPane sp = new JScrollPane(table);
+        
+        
+        if (tableWidth > width) {
+        	sp.setBounds(10, 11, (int)width - 15, (int)tableHeight + 1);
+        }
+        else {
+        	sp.setBounds(10, 11, (int)tableWidth, (int)tableHeight + 1);
+        }
+
+        
+        
+        seatChart.getContentPane().add(sp);
+        
+        seatChart.setVisible(true);
+        
+        JButton btnNewButton_2 = new JButton("OK");
+        btnNewButton_2.setBounds((sp.getWidth()/2 - 44), sp.getHeight() + 50, 89, 23);
+        seatChart.getContentPane().add(btnNewButton_2);
+        
+        
+        
+	}
+	
+    
+	
+	class CheckBoxCellRenderer implements TableCellRenderer {
+	    JComboBox combo;
+	    public CheckBoxCellRenderer(JComboBox comboBox) {
+	    this.combo = new JComboBox();
+	    for (int i=0; i<comboBox.getItemCount(); i++){
+	        combo.addItem(comboBox.getItemAt(i));
+	    }
+	    }
+	    public Component getTableCellRendererComponent(JTable jtable, 
+	                           Object value, 
+	                           boolean isSelected, 
+	                           boolean hasFocus, 
+	                           int row, int column) {
+	    combo.setSelectedItem(value);
+	    return combo;
+	    }
+	}
+}
